14f8699d0e57d2458cada081feccc404f3db3961
sophia <sophiendalo@gmail.com>
2023-03-18 16:56:40 +0300

    completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,10 +17,31 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+  
 
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant 
+      plant.update(plant_params)
+      render json: plant, status: :ok
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end  
+  end
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
 end
+
